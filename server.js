6f8fba6b99edb5404959887d71fb0ece18ca17a2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-30 08:05:54 +0530

    Update

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.json());
 
 app.use('/api', APIRoute);
 
+app.get('/', (req, res) => {
+    res.send(`Server running on port ${PORT}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
